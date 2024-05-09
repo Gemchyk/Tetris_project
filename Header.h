a83aff0829e923de692458d280e53d9c126b433f
Gemchyk <gemlifestudio@gmail.com>
2024-05-09 10:12:46 +0300

    Merge
    Merge

--- Header.h
+++ Header.h
@@ -7,6 +7,9 @@
 #define cell_size 50
 
 
+
+
+
 struct Box
 {
 	bool Status = false;
@@ -23,7 +26,19 @@ struct Figure
 	points pos;
 };
 
+std::vector<std::vector<Box>> figura(4, std::vector<Box>(4));
 
+void InitPalka()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int el = 0; el < 4; el++)
+		{
+			figura[0][i].Status = true;
+			figura[0][i].col = BLUE;
+		}
+	}
+}
 
 /*---------------------RANDOM----------------------*/
 
